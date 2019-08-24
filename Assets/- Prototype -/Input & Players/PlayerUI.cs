610aae9a731ec9424ce8acdf3bf31f5c5f77ab1c
nate.online <nate@travelwt.com>
2019-08-24 13:12:03 -0600

    Added station indicator

---
+++ Assets/- Prototype -/Input & Players/PlayerUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PlayerSystemControl))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerUI : MonoBehaviour
+{
+    public Text currentStationValue;
+    public Color32 activeColor, inactiveColor;
+
+    void Update()
+    {
+        if (GetComponent<PlayerSystemControl>().currentSystem != null)
+        {
+            currentStationValue.text = GetComponent<PlayerSystemControl>().currentSystem.gameObject.name;
+            currentStationValue.color = activeColor;
+        }
+        else
+        {
+            currentStationValue.text = "None";
+            currentStationValue.color = inactiveColor;
+        }
+    }
+}
