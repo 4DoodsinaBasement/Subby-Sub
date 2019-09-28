3dc29edb0c83f370556bb9561ef93a40c0b4c575
jt <j4c0bh3rrm4nn@gmail.com>
2019-09-28 16:29:07 -0600

    added Darkness script

---
+++ Assets/Darkness.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Darkness : MonoBehaviour
+{
+    public GameObject sub;
+    Light lt;
+   void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+       
+
+    // Update is called once per frame
+    void Update()
+    {
+        float depth = sub.transform.position.y;
+
+        if (depth >0)
+        {
+            lt.intensity = 1;
+        }
+        else
+        if (depth < -100)
+        {
+            lt.intensity = 0;
+        }
+        else{
+            lt.intensity = (depth * -1)/100;
+        }
+
+    
+    }
+}
