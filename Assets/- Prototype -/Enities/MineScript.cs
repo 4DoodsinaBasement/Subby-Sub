2018e91714754262176ce70c4c637361522ed11b
nate.online <nate@travelwt.com>
2019-11-02 16:53:31 -0600

    mines deal damage to the sub

---
+++ Assets/- Prototype -/Enities/MineScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineScript : MonoBehaviour
+{
+	public int baseDamage = 7;
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		SubController somePoorChap = other.gameObject.GetComponent<SubController>();
+
+		if (somePoorChap != null)
+		{
+			somePoorChap.ModifyHealth(-baseDamage);
+			Destroy(gameObject);
+		}
+	}
+}
