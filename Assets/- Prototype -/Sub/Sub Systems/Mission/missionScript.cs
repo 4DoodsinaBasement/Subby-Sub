6855e0f6aaf88d15f130fdecac2710ed57aff4c4
jt <j4c0bh3rrm4nn@gmail.com>
2019-11-07 20:06:07 -0700

    added Mission Subsystem

---
+++ Assets/- Prototype -/Sub/Sub Systems/Mission/missionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class missionScript : MonoBehaviour
+{
+    public GameObject MineCount; 
+    public Text MissionValueText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MissionValueText.text = MineCount.transform.childCount.ToString(); 
+    }
+}
