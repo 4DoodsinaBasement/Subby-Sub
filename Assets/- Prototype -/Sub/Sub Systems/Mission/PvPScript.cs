20fad75db37a5341b063b7177c2736637421d365
jt <j4c0bh3rrm4nn@gmail.com>
2019-11-07 21:56:37 -0700

    added mission info for PvP

---
+++ Assets/- Prototype -/Sub/Sub Systems/Mission/PvPScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PvPScript : MonoBehaviour
+{   
+    public GameObject EnemySub;
+    public Text MissionObjective;
+    public Text MissionValueText; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MissionObjective.text = "Defeat Enemy Sub";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MissionValueText.text = "Enemy Health: " + EnemySub.GetComponent<HPManager>().currentHP.ToString("F2");
+    }
+}
