afcec9b186e8418ecc057a981b07822eeb5688a3
nate.online <nate@travelwt.com>
2019-11-08 19:09:33 -0700

    Started work on new and improved sonar (just starting with entities)

---
+++ Assets/- Prototype -/Sub/Sub Systems/Sonar/EntitySonar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EntitySonar : MonoBehaviour
+{
+	public float sonarRange = 200f;
+
+	public List<SonarRenderer> sonarEntities = new List<SonarRenderer>();
+
+	void Start()
+	{
+		sonarEntities = FindObjectsOfType<SonarRenderer>().ToList();
+	}
+
+	void Update()
+	{
+
+	}
+}
