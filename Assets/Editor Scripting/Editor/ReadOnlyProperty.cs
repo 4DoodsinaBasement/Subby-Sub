729a93f56c65b704b1595cc3d72f383e454251c1
nate.online <nate@travelwt.com>
2021-06-29 18:00:14 -0600

    Made read only prop drawer only applicable in editor

--- Assets/Editor Scripting/Editor/ReadOnlyProperty.cs
+++ Assets/Editor Scripting/Editor/ReadOnlyProperty.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-
-public class ReadOnlyAttribute : PropertyAttribute {}
-
 [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
 public class ReadOnlyProperty : PropertyDrawer
 {
